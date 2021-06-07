4c58ddba28a6da1e578010427f5723fc52209ece
Michael Pierce <mpierce1138@gmail.com>
2021-06-07 12:28:23 -0300

    Renamed playerSelection to playerPlay, began playRound function.

--- test.js
+++ test.js
@@ -2,32 +2,44 @@ function computerPlay() {
     let randomIndex = Math.floor(Math.random() * 4);
     switch(randomIndex) {
         case 1:
+            console.log("Computer picked Rock.");
             return "rock";
         case 2:
+            console.log("Computer picked Paper.");
             return "paper";
         case 3:
+            console.log("Computer picked Scissors.");
             return "scissors";
         default:
             return null;
     }
 }
 
-function playerSelection(){
+function playerPlay(){
     let selectionString = prompt("Rock, paper or scissors:", "");
     selectionString = selectionString.toLowerCase();
 
     switch(true){
         case selectionString === "rock":
+            console.log("Player picked rock.");
             return "rock";
             break;
         case selectionString === "paper":
+            console.log("Player picked paper.");
             return "paper";
             break;
         case selectionString === "scissors":
+            console.log("Player picked scissors.");
             return "scissors";
             break;
         default:
+            console.log("Player entered invalid answer.");
             return null;
             break;
     }
 }
+
+function playRound(playerSelection, computerSelection) {
+
+}
+
